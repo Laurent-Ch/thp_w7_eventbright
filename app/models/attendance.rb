c7c84f71bb86bbf2e950904f6427b54ff209d8dc
VioMrqs <violette.marquis@gmail.com>
2021-11-10 11:25:05 +0100

    push heroku

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Attendance < ApplicationRecord
-  after_create :warning_host
+  #for Heroku
+  #after_create :warning_host
 
   belongs_to :guest, class_name: 'User'
   belongs_to :event
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_create :welcome_send
+
+  #for Heroku
+  #after_create :welcome_send
 
   has_many :attendances
   has_many :events, through: :attendances
